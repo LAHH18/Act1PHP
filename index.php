0064a905364f9bd063e06d386b93477f02ae6b45
Angel <luisangelhernandez02605@gmail.com>
2024-06-19 08:07:24 -0600

    PracticaPHP

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+echo "My first PHP script!";
+echo "<br>";
+echo "<br>";
+echo "Mis datos:";
+echo "<br>";
+$nombre = "Luis Angel Hdez Hdez";
+$edad = 18;
+$altura = 1.68;
+echo "Soy ". strtoupper($nombre) . ", tengo ". $edad ." años y mido ". $altura . " metros";
+echo "<br>";
+echo "<br>";
+var_dump($nombre);
+echo "<br>";
+echo "<br>";
+
+$x = 34;
+$y = 2;
+
+echo "OPERACIONES <br>";
+echo "La suma de $x + $y =". $x + $y ."<br>";
+echo "La resta de $x - $y =". $x - $y ."<br>";
+echo "La division de $x / $y =". $x / $y ."<br>";
+echo "La multiplicacion de $x * $y =". $x * $y ."<br>";
+echo "La exponenciacion de $x ** $y =". $x ** $y ."<br><br>";
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Practicas PHP</title>
+</head>
+<body>
+    Hola!!
+</body>
+</html>
